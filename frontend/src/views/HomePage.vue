2da78e06a32482ce888eb7395c4ba9ea9fb64d4c
Tae Woo Kim <taewookim02@gmail.com>
2025-02-11 13:36:26 +0900

    style: timedBestSeller btns

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,10 +1,11 @@
 <script setup>
-import HeroSection from '@/components/sections/HeroSection.vue';
-import ProductList from '@/components/sections/ProductList.vue';
-import ExampleSection from '@/components/sections/ExampleSection.vue';
-import CtaSection from '@/components/sections/CtaSection.vue';
+// import HeroSection from '@/components/sections/HeroSection.vue';
+// import ProductList from '@/components/sections/ProductList.vue';
+// import ExampleSection from '@/components/sections/ExampleSection.vue';
+// import CtaSection from '@/components/sections/CtaSection.vue';
 import ProductSliderList from '@/components/sections/ProductSliderList.vue';
 import HeroSection2 from '@/components/sections/HeroSection2.vue';
+import TimedBestSeller from '@/components/sections/TimedBestSeller.vue';
 
 // TODO: dtoList for each ProductSliderList
 
@@ -13,13 +14,18 @@ import HeroSection2 from '@/components/sections/HeroSection2.vue';
 </script>
 
 <template>
-  <!-- <HeroSection /> -->
-  <!-- <ProductList /> -->
-   <HeroSection2 />
-  <ProductSliderList slider-title="자격증 추천도서" />
-  <ProductSliderList slider-title="데이터 관련도서" />
-  <ExampleSection />
-  <CtaSection />
+    <!-- <HeroSection /> -->
+    <!-- <ProductList /> -->
+    <HeroSection2 />
+    <ProductSliderList slider-title="자격증 추천도서" />
+    <ProductSliderList slider-title="데이터 관련도서" />
+    <ProductSliderList slider-title="백엔드 관련도서" />
+    <ProductSliderList slider-title="프론트엔드 관련도서" />
+    <ProductSliderList slider-title="게임개발 관련도서" />
+    <ProductSliderList slider-title="AI 관련도서" />
+    <TimedBestSeller />
+    <!-- <ExampleSection />
+  <CtaSection /> -->
 </template>
 
 <style scoped></style>
